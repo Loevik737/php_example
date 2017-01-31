80a53667211cfe549c4bda14ee4fad6b2151cf91
Håvard Aspen Løvik <hloevi@live.no>
2017-01-31 21:32:45 +0100

    some small changes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Konsertbooking</title>
+  <title>User example</title>
 </head>
 <body>
 
